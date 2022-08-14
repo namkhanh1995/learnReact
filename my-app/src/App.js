4a3f91b3ce1c67b219caef0cbdf9b8a598d0c21a
DESKTOP-FF1278R\Admin <namkhanh18051995@gmail.com>
2022-08-14 22:28:32 +0700

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+      <div>Hello World
+        <DatePicker />
+      </div>
+  );
+}
+
+export default App;
