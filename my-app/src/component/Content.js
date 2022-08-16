610f6cd89265106eb0e9a91f340942e89de93702
khanh <namkhanh18051995@gmail.com>
2022-08-16 11:09:28 +0700

    done demo

--- my-app/src/component/Content.js
+++ my-app/src/component/Content.js
@@ -12,47 +12,36 @@ class Content extends Component {
           {id: 1, name : "IphoneX", price : "900", quantity:0 },
           {id: 2, name : "Samsung S9", price : "800", quantity:0 },
           {id: 3, name : "Nokia 8", price : "650", quantity:0 }
-        ]
+        ],
+        message: "" 
     }; 
   }
-  totalProduct(index){
+  callbackFunction = (childData,isCheckSum) => {
+    var sum = 0;
+    if(isCheckSum){
+      sum = this.state.total + parseInt(childData);
+    }else{
+      sum = this.state.total - parseInt(childData);
+    }
     this.setState((prevState, props)=>{
-      console.info(prevState.details[index].quantity);
-      return {
-        details: prevState.details.map(
-          obj => (obj.id === index + 1 ? Object.assign(obj, { quantity: prevState.details[index].quantity +0.5 }) : obj)
-        ),
-        total: prevState.total + 1 * prevState.details[index].price
-      }
-    });
-  }   
-  minusProduct(index){
-    this.setState((prevState, props)=>{
-      if (prevState.details[index].quantity !== 0){
-        console.info(prevState.details[index].quantity);
-        return {
-          details: prevState.details.map(
-            obj => (obj.id === index + 1 ? Object.assign(obj, { quantity: prevState.details[index].quantity - 0.5 }) : obj)
-          ),
-          total: prevState.total - 1 * prevState.details[index].price
-        }
-      }
-    });
-  }   
+      return {total: sum}
+    })
+  }
+
   render(){
     return (
         <Row gutter={[16, 16]}>
           <Col span={8}>
-            {/* {detailTags} */}
-            <Product name={this.state.details[0].name} price={this.state.details[0].price} quantity={this.state.details[0].quantity} addHandler={()=>this.totalProduct(0)} minusHandler={()=>this.minusProduct(0)}></Product>
+            <Product name={this.state.details[0].name} price={this.state.details[0].price} quantity={this.state.details[0].quantity} parentCallback = {this.callbackFunction}></Product>
           </Col>
           <Col span={8}>
-            <Product name={this.state.details[1].name} price={this.state.details[1].price} quantity={this.state.details[1].quantity} addHandler={()=>this.totalProduct(1)} minusHandler={()=>this.minusProduct(1)}></Product>
+            <Product name={this.state.details[1].name} price={this.state.details[1].price} quantity={this.state.details[1].quantity} parentCallback = {this.callbackFunction}></Product>
           </Col>
           <Col span={8}>
-            <Product name={this.state.details[2].name} price={this.state.details[2].price} quantity={this.state.details[2].quantity} addHandler={()=>this.totalProduct(2)} minusHandler={()=>this.minusProduct(2)}></Product>
+            <Product name={this.state.details[2].name} price={this.state.details[2].price} quantity={this.state.details[2].quantity} parentCallback = {this.callbackFunction}></Product>
           </Col>
           <Total total={this.state.total}></Total>
+          <p> {this.state.message} </p>
        </Row>
     );
   }
--- my-app/src/component/Product.js
+++ my-app/src/component/Product.js
@@ -2,20 +2,46 @@ import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 
 class Product extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name : this.props.name,
+            price : this.props.price,
+            quantity : this.props.quantity
+        };
+      }
+    totalProduct() {
+        this.setState((prevState, props)=>{
+          this.props.parentCallback(this.state.price,true);
+          return {
+            quantity : prevState.quantity + 1
+          }
+        });
+      }   
+    minusProduct(){
+      this.setState((prevState, props)=>{
+        if (prevState.quantity !== 0){
+          this.props.parentCallback(this.state.price,false);
+          return {
+            quantity : prevState.quantity - 1
+          }
+        }
+      });
+    } 
     render(){
         return (
         <div>
             <h1>
-                {this.props.name}
+                {this.state.name}
             </h1>
             <h2>
-                Price: {this.props.price} USD
+                Price: {this.state.price} USD
             </h2>
             <h2>
-                Quantity: {this.props.quantity}
+                Quantity: {this.state.quantity}
             </h2>
-            <button onClick={this.props.addHandler}>+</button>
-            <button onClick={this.props.minusHandler}>-</button>
+            <button onClick={()=>this.totalProduct()}>+</button>
+            <button onClick={()=>this.minusProduct()}>-</button>
       </div>
     );
   }
